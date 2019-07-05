e082b671941b3895351af34ce2c320c0c007edb0
Kroc Camen <kroc@camendesign.com>
2019-07-05 19:53:39 +0100

    Use lua to convert ASCII to C64 screen codes...
    Lua, because it's portable and can be included in the repo. Also, was super easy to learn.

--- build.bat
+++ build.bat
@@ -1,13 +1,26 @@
 @ECHO OFF
 CLS & TITLE Building Nucomer...
-::==============================================================================
 CD %~dp0
+
+REM # convert the article text into C64 text codes
+REM ============================================================================
+SET LUA="bin\lua\lua53.exe"
+SET LUA_TXT2C64=%LUA% "scripts\txt2c64.lua"
+
+%LUA_TXT2C64% "scripts\lorem-ipsum.txt" "build\lorem-ipsum.nu"
+
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+REM # assemble the outfit
+REM ============================================================================
 
 SET ACME="bin\acme\acme.exe" ^
     --format cbm ^
     --color ^
      -v9 ^
      -I "src"
+
+ECHO ?
 
 %ACME% ^
      --format plain ^
@@ -20,3 +33,6 @@ IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
                 "src\nucomer.acme"
 
 IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+REM # TODO: package disks
+REM ============================================================================
