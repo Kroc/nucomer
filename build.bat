af45f1541de5472a1ce4d141591f2981bb39bab5
Kroc Camen <kroc@camendesign.com>
2019-07-07 17:32:54 +0100

    Pack a test article into a disk-image
    We still need to read the file in from the C64 side to get it on screen, but we have at least solved the minimum requirement of getting text from the PC into a C64.

--- build.bat
+++ build.bat
@@ -2,17 +2,8 @@
 CLS & TITLE Building Nucomer...
 CD %~dp0
 
-REM # convert the article text into C64 text codes
-REM ============================================================================
 SET LUA="bin\lua\lua53.exe"
-SET LUA_TXT2C64=%LUA% "scripts\txt2c64.lua"
-
-%LUA_TXT2C64% "scripts\lorem-ipsum.txt" "build\lorem-ipsum.nu"
-
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
-
-REM # assemble the outfit
-REM ============================================================================
+SET LUA_TXT2C64=%LUA% "issues\txt2c64.lua"
 
 SET ACME="bin\acme\acme.exe" ^
     --format cbm ^
@@ -20,7 +11,28 @@ SET ACME="bin\acme\acme.exe" ^
      -v9 ^
      -I "src"
 
-ECHO ?
+SET C1541="bin\vice\c1541.exe"
+
+
+REM # convert the article text into C64 text codes
+REM ============================================================================
+
+%LUA_TXT2C64% "issues\lorem-ipsum.txt" "build\article.nu"
+
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+REM # wrap the raw binary data in a meta-data frame
+REM ----------------------------------------------------------------------------
+REM # this will give us a C64 PRG file we can then add to a disk-image 
+
+%ACME% ^
+     --outfile "build\lorem-ipsum.prg" ^
+     -- "issues\article.acme"
+
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+REM # assemble the outfit
+REM ============================================================================
 
 %ACME% ^
      --format plain ^
@@ -34,5 +46,14 @@ IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
 IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
-REM # TODO: package disks
+REM # package disks
 REM ============================================================================
+
+DEL "build\nucomer.d64"
+
+%C1541% ^
+     -format "nucomer,nu" d64 "build\nucomer.d64" ^
+     -write "build\nucomer.prg" "nucomer" ^
+     -write "build\lorem-ipsum.prg" "lorem-ipsum"
+
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
