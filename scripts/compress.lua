a56fce5ab1675512c80aabc7062cdba83fc875f3
Kroc Camen <kroc@camendesign.com>
2019-12-04 08:33:17 +0000

    Bit-pack spaces!
    If you remove the spaces from a sentence, there will be less entropy with character-pairs (=better compression). The hard part is putting the spaces back in afterwards.
    
    How do you add spaces back in though, when "the new" might tokenise the "e" & "n" together (without the space)?
    
    Answer: We bit-pack the spaces into bytes and consume a byte of spaces as we go. As the tokens unpack, we will eventually get a literal. Before printing the literal, we check out a byte of spaces -- 0 = space, 1 = solid -- and print all contiguous spaces until we reach a solid.
    
    Whenever 8 chars have been processed, we read another byte of spaces from the input -- the tricky part is encoding the stream so that the packed spaces are exactly in the right place where they will be *assumed* to be; no bits are ever wasted on "and now, some spaces".
    
    The algorithm on the PC-side to put the bit-packed spaces in the right places was hard work to conceptualise but it boils down to "for a token that expands to n literals, we will need as many bit-packed space bytes as contains n *non-spaces*.
    
    This whole thing is so crazy, I don't believe anybody else has done it before, at least on the C64.

--- scripts/compress.lua
+++ scripts/compress.lua
@@ -60,59 +60,6 @@ function Compress:addLine(src_line)
         -- version of the line when we begin compressing
         tokens = "",
     })
-
-    --[[
-    -- bit-pack the spaces
-    ----------------------------------------------------------------------------
-    local spaces = 0            -- current bit-pattern of spaces (8 bits)
-    local word = ""             -- we build a set of chars, without spaces
-    local out_chars = ""        -- compressed C64 text data
-
-    --#print('"'..line..'"')
-    --#io.stdout:write(".")
-
-    -- walk through the characters in the line
-    for i = 1, #src_chars do
-        ------------------------------------------------------------------------
-        -- is this character a space?
-        if src_chars:byte(i) == nuspc then
-            -- yes: set a bit in the spaces bitmap; the space
-            -- character doesn't need to be added to the 'word'
-            spaces = spaces * 2
-            spaces = spaces + 1
-            --#io.stdout:write("1")
-        else
-            -- no: add the character to the current 'word'
-            word = word .. src_chars:sub(i, i)
-            -- shift to the next space bit
-            -- without marking a space
-            spaces = spaces * 2
-            --#io.stdout:write("0")
-        end
-        -- is this the 8th character in a row?
-        if i % 8 == 0 then
-            --#io.stdout:write("\n.")
-            -- yes: output the spaces bitmap, and the 8 characters
-            out_chars = out_chars .. string.char(spaces) .. word
-            -- reset the word / spaces bitmap
-            spaces = 0
-            word = ""
-        end
-    end
-    -- when we reach the end of the line, we might not
-    -- have reached 8 charcters yet
-    if #word > 0 then
-        -- output the remaining characters
-        out_chars = out_chars .. string.char(spaces) .. word
-    end
-    --#io.stdout:write("\n")
-
-    -- TODO: pack 7 bits, and use the 8th to indicate "no spaces for 7 chars"?
-    if #out_chars > #src_chars then
-        print('"'..line..'"')
-        print("#", #src_chars, #out_chars)
-    end
-    --]]
 end
 
 -- compress the list of style classes into binary colour data
@@ -322,8 +269,9 @@ function Compress:compressLines()
     local token = self:tokeniseChars()
 
     io.stdout:write("> compressing...              ")
-    print()
 
+    -- iteratively pair tokens:
+    --
     while token <= 0xFF do
         ------------------------------------------------------------------------
         -- find the most common token pair
@@ -331,12 +279,12 @@ function Compress:compressLines()
         local old_pair, old_count = self:analysePairs()
 
         if old_count ~= 0 then
-            io.stdout:write(string.format(
-                "  ? $%02X = $%02X,$%02X ",
-                token,
-                old_pair:byte(1),
-                old_pair:byte(2)
-            ))
+            --#io.stdout:write(string.format(
+            --#    "  ? $%02X = $%02X,$%02X ",
+            --#    token,
+            --#    old_pair:byte(1),
+            --#    old_pair:byte(2)
+            --#))
         else
             break
         end
@@ -390,18 +338,56 @@ function Compress:compressLines()
             self.lines[i].tokens = new_line
         end
 
-        print(string.format(
-            " %3s %5d %10s",
-            self.tokens[token].size,
-            new_size-old_size,
-            filesize(new_size)
-        ))
+        --#print(string.format(
+        --#    " %3s %5d %10s",
+        --#    self.tokens[token].size,
+        --#    new_size-old_size,
+        --#    filesize(new_size)
+        --#))
 
         -- move to the next token number
         token = token + 1
     end
 
+    -- bit pack spaces!
+    ----------------------------------------------------------------------------
+    -- since we've removed the spaces, we need to put them back in
+    --
+    for i = 1, #self.lines do
+        ------------------------------------------------------------------------
+        -- there are no spaces on a blank line
+        -- (well, none that need printing anyway)
+        if #self.lines[i].tokens > 0 then
+            -- we will intersperse the space bytes with the tokens
+            local out_line = ""
+            local len = 0
+
+            -- walk each token in the line...
+            for j = 1, #self.lines[i].tokens do
+                ----------------------------------------------------------------
+                -- read a token
+                local tok = self.lines[i].tokens:byte(j)
+                -- place this token in the output
+                out_line = out_line .. string.char(tok)
+                -- how many characters will the token print?
+                len = len + self.tokens[tok].size
+                -- we will need as many space bytes
+                -- as contains that many non-spaces!
+                while len > 0 do
+                    -- fetch a byte of spaces
+                    local spc = table.remove(self.lines[i].spaces.bits, 1)
+                    -- add the byte of spaces into the output
+                    out_line = out_line .. string.char(spc)
+                    -- subtract the number of non-space characters
+                    len = len - self.bits[spc]
+                end
+            end
+            self.lines[i].tokens = out_line
+        end
+    end
+
     -- calculate the new size
+    ----------------------------------------------------------------------------
     local i_size = 0
     for _, line in ipairs(self.lines) do
         i_size = i_size + #line.colour + #line.tokens
@@ -435,25 +421,6 @@ function Compress:tokeniseChars()
         local j = 1
         while j <= #line.screen do
             --------------------------------------------------------------------
-            -- extract the spaces:
-            --
-            -- a _lot_ of text is just spaces between words. if instead we
-            -- represented the spaces as individual bits we could save a lot
-            -- of space. early word-processors would use the high-bit of a byte
-            -- to indicate the end of a word, and an implicit following space,
-            -- but we'll need all 256 screen-codes at our disposal
-            --
-            -- instead of a high-bit, we'll build a bitmap of the characters
-            -- in the line, where 0 = not a space and 1 = space. even though
-            -- a full line of 40 characters will need 5 bytes (40 bits), most
-            -- lines of such length already average five or more spaces whose
-            -- bytes won't be needed;
-            --
-            -- for now we only need to capture the column number of each space,
-            -- the bit-packing will occur during the compression stage
-            --
-            line.spaces[j] = (line.screen:byte(j) == str2scr[" "])
-
             -- count the character
             local char = line.screen:byte(j)
             self.chars[char] = (self.chars[char] or 0) + 1
@@ -466,11 +433,11 @@ function Compress:tokeniseChars()
     -- the order used is not actually important, but could perhaps be
     -- controlled in the future as some kind of arithmetic-coding
     --
-    local token = 1
+    local token = 0
+
     -- check each of the possible unqiue screen codes...
-    -- NOTE: beginning at 1 skips the space (0x00) character
     --
-    for i = 1, 255 do
+    for i = 0, 255 do
         ------------------------------------------------------------------------
         -- was the screen-code ever used?
         if self.chars[i] > 0 then
@@ -497,20 +464,50 @@ function Compress:tokeniseChars()
 
         -- this will be the token-encoded version of the line
         local tok_line = ""
-        -- walk each screen-code in the source line
+        -- used for bit-packing the spaces
+        local spc_cols = {}
+        local spc_bits = {}
+        local spc      = 0
+
+        -- walk each screen-code
+        -- in the source line:
+        --
         for j = 1, #scr_line do
+            --------------------------------------------------------------------
+            -- is this the 8th character in a row?
+            if j > 1 and (j-1) % 8 == 0 then
+                -- yes: add the byte to the spaces bitmap
+                table.insert(spc_bits, spc)
+                -- reset the word / spaces bitmap
+                spc = 0
+            end
             -- determine the screen code
             local scr = scr_line:byte(j)
-            -- don't include spaces in the token output
-            if scr ~= 0x00 then
-                -- get the token for it
+            -- is this a space? extract and bit-pack
+            if scr == str2scr[" "] then
+                -- mark column as a space
+                -- (leave a zero in the space bitmap)
+                spc_cols[j] = true
+            else
+                -- not a space
+                spc_cols[j] = false
+                -- set the bit in the space bitmap
+                spc = spc + 2 ^ ((j-1) % 8) | 0
+                -- get the token for the screen-code
                 local tok = self.chars[scr]
                 -- add the token to the output
                 tok_line = tok_line .. string.char(tok)
             end
         end
+        -- add the last byte of spaces
+        table.insert(spc_bits, spc)
+
         -- the line has been converted
         self.lines[i].tokens = tok_line
+        self.lines[i].spaces = {
+            cols = spc_cols,
+            bits = spc_bits
+        }
     end
 
     -- return the next available token number,
@@ -746,5 +743,281 @@ lines:
 
     return s_out
 end
+
+-- a table of the number of bits in a byte
+--
+Compress.bits = {
+    [0x00] = 0, -- %00000000
+    [0x01] = 1, -- %00000001
+    [0x02] = 1, -- %00000010
+    [0x03] = 2, -- %00000011
+    [0x04] = 1, -- %00000100
+    [0x05] = 2, -- %00000101
+    [0x06] = 2, -- %00000110
+    [0x07] = 3, -- %00000111
+    [0x08] = 1, -- %00001000
+    [0x09] = 2, -- %00001001
+    [0x0a] = 2, -- %00001010
+    [0x0b] = 3, -- %00001011
+    [0x0c] = 2, -- %00001100
+    [0x0d] = 3, -- %00001101
+    [0x0e] = 3, -- %00001110
+    [0x0f] = 4, -- %00001111
+
+    [0x10] = 1, -- %00010000
+    [0x11] = 2, -- %00010001
+    [0x12] = 2, -- %00010010
+    [0x13] = 3, -- %00010011
+    [0x14] = 2, -- %00010100
+    [0x15] = 3, -- %00010101
+    [0x16] = 3, -- %00010110
+    [0x17] = 4, -- %00010111
+    [0x18] = 2, -- %00011000
+    [0x19] = 3, -- %00011001
+    [0x1a] = 3, -- %00011010
+    [0x1b] = 4, -- %00011011
+    [0x1c] = 3, -- %00011100
+    [0x1d] = 4, -- %00011101
+    [0x1e] = 4, -- %00011110
+    [0x1f] = 5, -- %00011111
+
+    [0x20] = 1, -- %00100000
+    [0x21] = 2, -- %00100001
+    [0x22] = 2, -- %00100010
+    [0x23] = 3, -- %00100011
+    [0x24] = 2, -- %00100100
+    [0x25] = 3, -- %00100101
+    [0x26] = 3, -- %00100110
+    [0x27] = 4, -- %00100111
+    [0x28] = 2, -- %00101000
+    [0x29] = 3, -- %00101001
+    [0x2a] = 3, -- %00101010
+    [0x2b] = 4, -- %00101011
+    [0x2c] = 3, -- %00101100
+    [0x2d] = 4, -- %00101101
+    [0x2e] = 4, -- %00101110
+    [0x2f] = 5, -- %00101111
+
+    [0x30] = 2, -- %00110000
+    [0x31] = 3, -- %00110001
+    [0x32] = 3, -- %00110010
+    [0x33] = 4, -- %00110011
+    [0x34] = 3, -- %00110100
+    [0x35] = 4, -- %00110101
+    [0x36] = 4, -- %00110110
+    [0x37] = 5, -- %00110111
+    [0x38] = 3, -- %00111000
+    [0x39] = 4, -- %00111001
+    [0x3a] = 4, -- %00111010
+    [0x3b] = 5, -- %00111011
+    [0x3c] = 4, -- %00111100
+    [0x3d] = 5, -- %00111101
+    [0x3e] = 5, -- %00111110
+    [0x3f] = 6, -- %00111111
+
+    [0x40] = 1, -- %01000000
+    [0x41] = 2, -- %01000001
+    [0x42] = 2, -- %01000010
+    [0x43] = 3, -- %01000011
+    [0x44] = 2, -- %01000100
+    [0x45] = 3, -- %01000101
+    [0x46] = 3, -- %01000110
+    [0x47] = 4, -- %01000111
+    [0x48] = 2, -- %01001000
+    [0x49] = 3, -- %01001001
+    [0x4a] = 3, -- %01001010
+    [0x4b] = 4, -- %01001011
+    [0x4c] = 3, -- %01001100
+    [0x4d] = 4, -- %01001101
+    [0x4e] = 4, -- %01001110
+    [0x4f] = 5, -- %01001111
+
+    [0x50] = 2, -- %01010000
+    [0x51] = 3, -- %01010001
+    [0x52] = 3, -- %01010010
+    [0x53] = 4, -- %01010011
+    [0x54] = 3, -- %01010100
+    [0x55] = 4, -- %01010101
+    [0x56] = 4, -- %01010110
+    [0x57] = 5, -- %01010111
+    [0x58] = 3, -- %01011000
+    [0x59] = 4, -- %01011001
+    [0x5a] = 4, -- %01011010
+    [0x5b] = 5, -- %01011011
+    [0x5c] = 4, -- %01011100
+    [0x5d] = 5, -- %01011101
+    [0x5e] = 5, -- %01011110
+    [0x5f] = 6, -- %01011111
+
+    [0x60] = 2, -- %01100000
+    [0x61] = 3, -- %01100001
+    [0x62] = 3, -- %01100010
+    [0x63] = 4, -- %01100011
+    [0x64] = 3, -- %01100100
+    [0x65] = 4, -- %01100101
+    [0x66] = 4, -- %01100110
+    [0x67] = 5, -- %01100111
+    [0x68] = 3, -- %01101000
+    [0x69] = 4, -- %01101001
+    [0x6a] = 4, -- %01101010
+    [0x6b] = 5, -- %01101011
+    [0x6c] = 4, -- %01101100
+    [0x6d] = 5, -- %01101101
+    [0x6e] = 5, -- %01101110
+    [0x6f] = 6, -- %01101111
+
+    [0x70] = 3, -- %01110000
+    [0x71] = 4, -- %01110001
+    [0x72] = 4, -- %01110010
+    [0x73] = 5, -- %01110011
+    [0x74] = 4, -- %01110100
+    [0x75] = 5, -- %01110101
+    [0x76] = 5, -- %01110110
+    [0x77] = 6, -- %01110111
+    [0x78] = 4, -- %01111000
+    [0x79] = 5, -- %01111001
+    [0x7a] = 5, -- %01111010
+    [0x7b] = 6, -- %01111011
+    [0x7c] = 5, -- %01111100
+    [0x7d] = 6, -- %01111101
+    [0x7e] = 6, -- %01111110
+    [0x7f] = 7, -- %01111111
+
+    [0x80] = 1, -- %10000000
+    [0x81] = 2, -- %10000001
+    [0x82] = 2, -- %10000010
+    [0x83] = 3, -- %10000011
+    [0x84] = 2, -- %10000100
+    [0x85] = 3, -- %10000101
+    [0x86] = 3, -- %10000110
+    [0x87] = 4, -- %10000111
+    [0x88] = 2, -- %10001000
+    [0x89] = 3, -- %10001001
+    [0x8a] = 3, -- %10001010
+    [0x8b] = 4, -- %10001011
+    [0x8c] = 3, -- %10001100
+    [0x8d] = 4, -- %10001101
+    [0x8e] = 4, -- %10001110
+    [0x8f] = 5, -- %10001111
+
+    [0x90] = 2, -- %10010000
+    [0x91] = 3, -- %10010001
+    [0x92] = 3, -- %10010010
+    [0x93] = 4, -- %10010011
+    [0x94] = 3, -- %10010100
+    [0x95] = 4, -- %10010101
+    [0x96] = 4, -- %10010110
+    [0x97] = 5, -- %10010111
+    [0x98] = 3, -- %10011000
+    [0x99] = 4, -- %10011001
+    [0x9a] = 4, -- %10011010
+    [0x9b] = 5, -- %10011011
+    [0x9c] = 4, -- %10011100
+    [0x9d] = 5, -- %10011101
+    [0x9e] = 5, -- %10011110
+    [0x9f] = 6, -- %10011111
+
+    [0xa0] = 2, -- %10100000
+    [0xa1] = 3, -- %10100001
+    [0xa2] = 3, -- %10100010
+    [0xa3] = 4, -- %10100011
+    [0xa4] = 3, -- %10100100
+    [0xa5] = 4, -- %10100101
+    [0xa6] = 4, -- %10100110
+    [0xa7] = 5, -- %10100111
+    [0xa8] = 3, -- %10101000
+    [0xa9] = 4, -- %10101001
+    [0xaa] = 4, -- %10101010
+    [0xab] = 5, -- %10101011
+    [0xac] = 4, -- %10101100
+    [0xad] = 5, -- %10101101
+    [0xae] = 5, -- %10101110
+    [0xaf] = 6, -- %10101111
+
+    [0xb0] = 3, -- %10110000
+    [0xb1] = 4, -- %10110001
+    [0xb2] = 4, -- %10110010
+    [0xb3] = 5, -- %10110011
+    [0xb4] = 4, -- %10110100
+    [0xb5] = 5, -- %10110101
+    [0xb6] = 5, -- %10110110
+    [0xb7] = 6, -- %10110111
+    [0xb8] = 4, -- %10111000
+    [0xb9] = 5, -- %10111001
+    [0xba] = 5, -- %10111010
+    [0xbb] = 6, -- %10111011
+    [0xbc] = 5, -- %10111100
+    [0xbd] = 6, -- %10111101
+    [0xbe] = 6, -- %10111110
+    [0xbf] = 7, -- %10111111
+
+    [0xc0] = 2, -- %11000000
+    [0xc1] = 3, -- %11000001
+    [0xc2] = 3, -- %11000010
+    [0xc3] = 4, -- %11000011
+    [0xc4] = 3, -- %11000100
+    [0xc5] = 4, -- %11000101
+    [0xc6] = 4, -- %11000110
+    [0xc7] = 5, -- %11000111
+    [0xc8] = 3, -- %11001000
+    [0xc9] = 4, -- %11001001
+    [0xca] = 4, -- %11001010
+    [0xcb] = 5, -- %11001011
+    [0xcc] = 4, -- %11001100
+    [0xcd] = 5, -- %11001101
+    [0xce] = 5, -- %11001110
+    [0xcf] = 6, -- %11001111
+
+    [0xd0] = 3, -- %11010000
+    [0xd1] = 4, -- %11010001
+    [0xd2] = 4, -- %11010010
+    [0xd3] = 5, -- %11010011
+    [0xd4] = 4, -- %11010100
+    [0xd5] = 5, -- %11010101
+    [0xd6] = 5, -- %11010110
+    [0xd7] = 6, -- %11010111
+    [0xd8] = 4, -- %11011000
+    [0xd9] = 5, -- %11011001
+    [0xda] = 5, -- %11011010
+    [0xdb] = 6, -- %11011011
+    [0xdc] = 5, -- %11011100
+    [0xdd] = 6, -- %11011101
+    [0xde] = 6, -- %11011110
+    [0xdf] = 7, -- %11011111
+
+    [0xe0] = 3, -- %11100000
+    [0xe1] = 4, -- %11100001
+    [0xe2] = 4, -- %11100010
+    [0xe3] = 5, -- %11100011
+    [0xe4] = 4, -- %11100100
+    [0xe5] = 5, -- %11100101
+    [0xe6] = 5, -- %11100110
+    [0xe7] = 6, -- %11100111
+    [0xe8] = 4, -- %11101000
+    [0xe9] = 5, -- %11101001
+    [0xea] = 5, -- %11101010
+    [0xeb] = 6, -- %11101011
+    [0xec] = 5, -- %11101100
+    [0xed] = 6, -- %11101101
+    [0xee] = 6, -- %11101110
+    [0xef] = 7, -- %11101111
+
+    [0xf0] = 4, -- %11110000
+    [0xf1] = 5, -- %11110001
+    [0xf2] = 5, -- %11110010
+    [0xf3] = 6, -- %11110011
+    [0xf4] = 5, -- %11110100
+    [0xf5] = 6, -- %11110101
+    [0xf6] = 6, -- %11110110
+    [0xf7] = 7, -- %11110111
+    [0xf8] = 5, -- %11111000
+    [0xf9] = 6, -- %11111001
+    [0xfa] = 6, -- %11111010
+    [0xfb] = 7, -- %11111011
+    [0xfc] = 6, -- %11111100
+    [0xfd] = 7, -- %11111101
+    [0xfe] = 7, -- %11111110
+    [0xff] = 8, -- %11111111
+}
 
 return Compress
